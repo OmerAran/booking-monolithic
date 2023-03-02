0e180d62ad6958627bbb59b7c7ee98142f299d34
OmerAran <omerr.aran@gmail.com>
2023-03-02 23:04:22 +0300

    added cross origin

--- src/main/java/com/omeraran/booking/controller/MovieController.java
+++ src/main/java/com/omeraran/booking/controller/MovieController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/movie")
+@CrossOrigin("http://localhost:3000/")
 public class MovieController {
     
     private final MovieService movieService;
